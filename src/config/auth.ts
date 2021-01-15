b5b986f96208c0a1819c55fce48665d22ec15228
Monan Bertoluci <monan.bertoluci.c@conceptatech.com>
2021-01-15 09:51:01 -0300

    node-backend-simple-project

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'bb11b6acef4ba315a43dee41925d49f1',
+    expiresIn: '1d',
+  }
+}
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+const sessionsRouter = Router();
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+  const authenticateUser = new AuthenticateUserService();
+  const { user, token } = await authenticateUser.execute({ email, password });
+  return response.json({user, token});
+});
+
+export default sessionsRouter;
